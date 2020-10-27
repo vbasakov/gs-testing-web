2233413cbbd1e3078c52ec2563f025c327e685e9
vbasakov <vbasakov@gmail.com>
2020-10-27 08:36:33 +0300

    parametrized request test

--- complete/src/main/java/com/example/testingweb/HomeController.java
+++ complete/src/main/java/com/example/testingweb/HomeController.java
@@ -1,15 +1,19 @@
 package com.example.testingweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public @ResponseBody String greeting() {
-		return "Hello, World";
+	public @ResponseBody String greeting(
+			@RequestParam(defaultValue = "Default", required = false) String param) {
+
+		return "home" + param;
 	}
 
 }
--- complete/src/test/java/com/example/testingweb/HttpRequestTest.java
+++ complete/src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -21,7 +23,15 @@ public class HttpRequestTest {
 
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("Hello, World");
+		String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+		assertThat(response).contains("homeDefault");
+	}
+
+	@Test
+	public void greetingShouldReturnParametrizedMessage() throws Exception {
+		String param1 = "Param";
+		String url = "http://localhost:" + port + "/?param="+param1;
+		String response = this.restTemplate.getForObject(url, String.class);
+		assertThat(response).contains("home"+param1);
 	}
 }
